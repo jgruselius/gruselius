e32c15f2321146e46b1b9445d42b911cb198fb09
Joel G <joel.gruselius@scilifelab.se>
2013-02-12 21:57:03 +0100

    Fixed URL for fetching commits

--- gruselius/session4.py
+++ gruselius/session4.py
@@ -12,7 +12,7 @@ org_url = "https://api.github.com/orgs/pythonkurs"
 repo_data = requests.get(org_url+"/repos", auth=(user, password)).json()
 d = {}
 for repo in repo_data:
-	url = repo["url"]+"commits/"
+	url = repo["url"]+"/commits"
 	commit_data = requests.get(url, auth=(user, password)).json()
 	dates = []
 	messages = []

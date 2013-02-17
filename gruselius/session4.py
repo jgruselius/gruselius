3bfcaef6fe47a22ac580dd378b679573792746ed
Joel G <joel.gruselius@scilifelab.se>
2013-02-17 22:08:23 +0100

    Added function for calculating date of most commits

--- gruselius/session4.py
+++ gruselius/session4.py
@@ -4,24 +4,29 @@ from pandas import Series
 from pandas import DataFrame
 import getpass
 
-# Temporary solution for getting user/pw:
-user = raw_input("GitHub username:")
-password = getpass.getpass("Password:")
+def input_credentials():
+	user = raw_input("GitHub username:")
+	password = getpass.getpass("Password:")
+	return (user, password)
 
-org_url = "https://api.github.com/orgs/pythonkurs"
-repo_data = requests.get(org_url+"/repos", auth=(user, password)).json()
-d = {}
-for repo in repo_data:
-	url = repo["url"]+"/commits"
-	commit_data = requests.get(url, auth=(user, password)).json()
-	dates = []
-	messages = []
-	if len(commit_data) > 1:
-		for commit in commit_data:
-			dates.append(parser.parse(commit["commit"]["committer"]["date"]))
-			messages.append(commit["commit"]["message"])
-		d[repo["name"]] = Series(messages, index=dates)
 
-df = DataFrame(d)
-print(df)
+def get_commit_history():
+	user, password = input_credentials()
+	org_url = "https://api.github.com/orgs/pythonkurs"
+	repo_data = requests.get(org_url+"/repos", auth=(user, password)).json()
+	d = {}
+	for repo in repo_data:
+		url = repo["url"]+"/commits"
+		commit_data = requests.get(url, auth=(user, password)).json()
+		dates = []
+		messages = []
+		if len(commit_data) > 1:
+			for commit in commit_data:
+				dates.append(parser.parse(commit["commit"]["committer"]["date"]))
+				messages.append(commit["commit"]["message"])
+			d[repo["name"]] = Series(messages, index=dates)
 
+	return DataFrame(d)
+
+def get_date_of_most_commits(df):
+	df.count(1).resample("D",how="sum").idxmax()

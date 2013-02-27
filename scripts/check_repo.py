f3882669f8d9477953bf998a377cdddbaf4b4e81
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-02-27 11:32:17 +0100

    Finished session 6 assignment

--- scripts/check_repo.py
+++ scripts/check_repo.py
@@ -3,13 +3,14 @@ import sys
 import os
 from gruselius.session3 import CourseRepo, TemporarilyChangeDir
 
+#@profile
 def main(args):
-	if len(args) != 1:
+	if len(args) < 2:
 		help()
-	elif not os.path.exists(args[0]):
-		print("The path '{0}' does not exist".format(args[0]))
+	elif not os.path.exists(args[-1]):
+		print("The path '{0}' does not exist".format(args[-1]))
 	else:
-		path = os.path.abspath(args[0])
+		path = os.path.abspath(args[-1])
 		with TemporarilyChangeDir(path):
 			cr = CourseRepo(os.path.basename(path))
 			if cr.check():
@@ -21,4 +22,4 @@ def help():
 	print("Usage:\n\tcheck_repo.py <path>\n")
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	main(sys.argv)

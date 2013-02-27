6d55db4f61e41a313d5eaffbea2a8fc0c218bea3
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-02-27 12:46:55 +0100

    argparse used for handling arguments

--- scripts/check_repo.py
+++ scripts/check_repo.py
@@ -1,16 +1,14 @@
 
-import sys
+import argparse
 import os
 from gruselius.session3 import CourseRepo, TemporarilyChangeDir
 
 #@profile
-def main(args):
-	if len(args) < 2:
-		help()
-	elif not os.path.exists(args[-1]):
-		print("The path '{0}' does not exist".format(args[-1]))
+def main(path):
+	if not os.path.exists(path):
+		print("The path '{0}' does not exist".format(path))
 	else:
-		path = os.path.abspath(args[-1])
+		path = os.path.abspath(path)
 		with TemporarilyChangeDir(path):
 			cr = CourseRepo(os.path.basename(path))
 			if cr.check():
@@ -18,8 +16,10 @@ def main(args):
 			else:
 				print("FAIL")
 
-def help():
-	print("Usage:\n\tcheck_repo.py <path>\n")
-
 if __name__ == "__main__":
-	main(sys.argv)
+	# To work with profilers the last argument is used as path
+	p = argparse.ArgumentParser(description="Checks if the specified path" +
+		"contains the required files")
+	p.add_argument("path", help="The path to check", nargs="+")
+	args = p.parse_args()
+	main(args.path[-1])

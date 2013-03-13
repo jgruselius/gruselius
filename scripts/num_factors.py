2c65e7c412b0c3bdf616e71be9ba7b798bd038f0
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-03-13 16:59:29 +0100

    Added some code to num_factors.py

--- scripts/num_factors.py
+++ scripts/num_factors.py
@@ -1,10 +1,49 @@
 """Python course session 7 assignment"""
 
 import argparse
+import time
+import multiprocessing
+#import iPython.parallel
+
+RANGE = (2,50000)
+
+def factorize(n):
+	if n < 2:
+		return []
+	factors = []
+	p = 2
+	while True:
+		if n == 1:
+			return factors
+		r = n % p
+		if r == 0:
+			factors.append(p)
+			n = n / p
+		elif p * p >= n:
+			factors.append(n)
+			return factors
+		elif p > 2:
+			p += 2
+		else:
+			p += 1
 
 def function1():
+	t0 = time.clock()
 	"""Sequential non-parallel function"""
-	pass
+	factor_count = {}
+	fco = {}
+	for i in xrange(*RANGE):
+		count = len(set(factorize(i)))
+		factor_count[i] = count
+		if count in fco:
+			fco[count] += 1
+		else:
+			fco[count] = 1
+	""" 
+	factors = set([factor for i in xrange(RANGE) for factor in factorize(i)])
+	"""
+	print("{}".format(time.clock()-t0))
+	return fco
 
 def function2():
 	"""Parallelized using multiprocessing"""
@@ -19,7 +58,7 @@ def main(arg):
 	if not arg in modes:
 		raise NameError
 	else:
-		modes[arg]()
+		print(modes[arg]())
 
 if __name__ == "__main__":
 	p = argparse.ArgumentParser("...")

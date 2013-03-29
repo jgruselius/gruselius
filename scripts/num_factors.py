c542b947da0d8a38282825cc8de17d80b3d93751
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-03-29 19:08:47 +0100

    Changed functions

--- scripts/num_factors.py
+++ scripts/num_factors.py
@@ -3,9 +3,22 @@
 import argparse
 import time
 import multiprocessing
-#import iPython.parallel
+from IPython.parallel import Client
 
-RANGE = (2,500000)
+RANGE = (2,50000)
+
+"""
+Build a collection of all unique factors:
+Method 1, building a set (8.9 s):
+unique_factors = set()
+for i in xrange(*RANGE):
+	unique_factors.update(factorize(i))
+Method 2, set from list comprehension (8.7 s):
+unique_factors = set([factor for i in xrange(*RANGE) for factor in factorize(i)])
+Method 3, set from map (9.33 s):
+unique_factors = set([factor for factors in map(factorize, xrange(*RANGE)) for factor in factors])
+return len(unique_factors)
+"""
 
 def factorize(n):
 	if n < 2:
@@ -27,46 +40,31 @@ def factorize(n):
 		else:
 			p += 1
 
-def unique_factors(iterable):
-	# Unique factors: [uf for i in iterable for uf in set(factorize(i))]
-	# Unique factor count:
-	return [len(set(factorize(i))) for i in iterable]
+def number_of_unique_factors(n):
+	return len(set(factorize(n)))
+
+def count_unique(iterable):
+	counts = {}
+	for n in iterable:
+		if n in counts:
+			counts[n] += 1
+		else:
+			counts[n] = 1
+	return counts
 
 def function1():
-	t0 = time.clock()
 	"""Sequential non-parallel function"""
-	"""
-	factor_count = {}
-	fco = {} # factor count occurances
-	for i in xrange(*RANGE):
-		count = len(set(factorize(i)))
-		factor_count[i] = count
-		if count in fco:
-			fco[count] += 1
-		else:
-			fco[count] = 1
-	# return fco
-	"""
-	# Build a collection of all unique factors:
-	# Method 1, building a set (8.9 s):
-	# unique_factors = set()
-	# for i in xrange(*RANGE):
-	# 	unique_factors.update(factorize(i))
-	# Method 2, set from list comprehension (8.7 s):
-	unique_factors = set([factor for i in xrange(*RANGE) for factor in factorize(i)])
-	# Method 3, set from map (9.33 s):
-	# unique_factors = set([factor for factors in map(factorize, xrange(*RANGE)) for factor in factors])
-	return len(unique_factors)
+	return [number_of_unique_factors(i) for i in xrange(*RANGE)]
 
 def function2():
 	"""Parallelized using multiprocessing"""
 	pool = multiprocessing.Pool()
-	result = set([factor for factors in pool.map(factorize,xrange(*RANGE)) for factor in factors])
-	return len(result)
+	return pool.map(number_of_unique_factors, xrange(*RANGE))
 
 def function3():
 	"""Parallelized using iPython"""
-	pass
+	dview = Client()[:]
+	return dview.map(number_of_unique_factors, xrange(*RANGE))
 
 def main(arg):
 	modes = {"s": function1, "m": function2, "i": function3}
@@ -74,7 +72,7 @@ def main(arg):
 		raise NameError
 	else:
 		t0 = time.time()
-		print(modes[arg]())
+		print(count_unique(modes[arg]()))
 		print("Timer: {0} s".format(time.time()-t0))
 
 if __name__ == "__main__":
